b1ae07dbd3c1e64df2c70f06c3f5169e46924978
krzysiek <kkos8@wp.pl>
2021-04-28 19:40:33 +0200

    Add another lesson about functions js

---
+++ script.js
@@ -0,0 +1,14 @@
+let names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert', 'Jada']
+let para = document.createElement('p');
+let first = 0;
+let last = names.length-1;
+
+ let chooseName = function(last, first){
+  return Math.floor(Math.random() * (last - first) + first);
+}
+para.textContent = names[chooseName(last, first)];
+
+
+const section = document.querySelector('section');
+
+section.appendChild(para);
